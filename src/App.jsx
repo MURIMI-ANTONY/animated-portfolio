18fac931a30e35579767a0d2fc5a46afa1734eb8
MURIMI-ANTONY <amurimi101@gmail.com>
2024-06-13 19:26:44 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import 'tailwindcss/tailwind.css';
+
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div className=''>
+    <section className=" ">Hero</section>
+    <section className="">Parallax</section>
+    <section className="">Services</section>
+    <section className="">Parallax</section>
+    <section className="">Portfolio3</section>
+    <section className="">Portfolio3</section>
+    <section className="">Portfolio3</section>
+    <section className="">Contact</section>
+  </div>;
 };
 
 export default App;
